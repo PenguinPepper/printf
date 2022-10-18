addeb5f55c8d4ea05f9cd996b7220a2e0274a357
Elizabeth <testlolabtest@gmail.com>
2022-10-18 12:49:23 -0700

    Add first file for task 0

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _printf - functio that produces output according to format
+ * @format: is a character string which is composed of zero or more directives
+ *
+ * Description: write output to stdout, the standard output stream
+ * Return: the number of characters printed (excluding
+ * the null byte used to end output to strings)
+ */
+int _printf(const char *format, ...)
+{
+
+}
