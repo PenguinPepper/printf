7c95c78b06e97330fa95e6ac7a09afc19caeac35
Elizabeth <testlolabtest@gmail.com>
2022-10-19 06:32:19 -0700

    Add function statemnts to _printf function

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _printf - functio that produces output according to format
+ * _printf - function that produces output according to format
  * @format: is a character string which is composed of zero or more directives
  *
  * Description: write output to stdout, the standard output stream
@@ -10,5 +10,15 @@
  */
 int _printf(const char *format, ...)
 {
+	va_list args;
 
+	va_start(args, format);
+
+	/*
+	 * function for printing formated data from
+	 * variable argument list to stdout
+	*/
+	_vprintf(format, args);
+
+	va_end(format);
 }
